624026699fe4b4e52d3483d8dc06334e764c5cbb
Roman Dyukachov <roman.dyukachov@spryrocks.com>
2018-08-18 13:08:25 +0300

    Implement hide keyboard functionality for Emdpoints

--- Sources/AndroidModules/android.modules.ui.routing/src/main/java/com/spryrocks/android/modules/ui/routing/endpoints/FrameEndpointSettings.java
+++ Sources/AndroidModules/android.modules.ui.routing/src/main/java/com/spryrocks/android/modules/ui/routing/endpoints/FrameEndpointSettings.java
@@ -2,13 +2,15 @@ package com.spryrocks.android.modules.ui.routing.endpoints;
 
 public class FrameEndpointSettings {
     boolean clearBackStack;
+    boolean hideKeyboard;
 
     FrameEndpointSettings() {
     }
 
     @SuppressWarnings({"UnusedReturnValue", "SameParameterValue", "unused"})
-    public FrameEndpointSettings clearBackStack(boolean clearBackStack) {
+    public FrameEndpointSettings clearBackStack(boolean clearBackStack, boolean hideKeyboard) {
         this.clearBackStack = clearBackStack;
+        this.hideKeyboard = hideKeyboard;
         return this;
     }
 }
